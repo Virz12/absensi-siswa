3c5cd70ed3e51135b1ea7c6a154f2f7a0031ebc2
Frmsfwn <hammerrmx1320@gmail.com>
2024-05-27 10:33:56 +0700

    Update Route & Remove Unnecessary File

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,66 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $data_siswa = [
+            [
+                'id' => '1',
+                'username' => 'rifqi',
+                'Role' => 'siswa',
+                'telefone' => '',
+                'jenis_kelamin' => '',
+                'status' => '',
+                'password' => bcrypt('rfq1611')
+            ],[
+                'id' => '2',
+                'username' => 'virgi',
+                'Role' => 'siswa',
+                'telefone' => '',
+                'jenis_kelamin' => '',
+                'status' => '',
+                'password' => bcrypt('virz')
+            ],[
+                'id' => '3',
+                'username' => 'fajar',
+                'Role' => 'siswa',
+                'telefone' => '',
+                'jenis_kelamin' => '',
+                'status' => '',
+                'password' => bcrypt('fazar')
+            ],[
+                'id' => '4',
+                'username' => 'zulfan',
+                'Role' => 'siswa',
+                'telefone' => '',
+                'jenis_kelamin' => '',
+                'status' => '',
+                'password' => bcrypt('panjul')
+            ]
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $data_Admin = [
+            [
+                'id' => '5',
+                'username' => 'admin',
+                'Role' => 'admin',
+                'telefone' => '',
+                'jenis_kelamin' => '',
+                'status' => '',
+                'password' => bcrypt('admin')
+            ]
+        ];
+
+        foreach($data_siswa as $data) {
+            user::create($data);
+        }
+
+        foreach($data_Admin as $data) {
+            user::create($data);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,26 @@ use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Auth;
 
 Route::middleware(['preventBackHistory','guest'])->group(function () {
+    Route::get('/', function () {
+        return redirect('/login');
+    });
     Route::get('/login', [LoginController::class, 'login'])->name('login');
     Route::post('/login', [LoginController::class, 'storelogin']);
 });
 
 Route::middleware(['preventBackHistory','auth'])->group(function () {
+    Route::get('/home', function() {
+        if (Auth::user()->Role == 'admin') {
+            return redirect('/dashboard');
+        }elseif (Auth::user()->Role == 'siswa') {
+            return redirect('/absen');
+        }
+    });
     Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 });
 
 //Admin
 Route::middleware(['preventBackHistory','auth','userAccess:admin'])->group(function () {
-    
     Route::get('/dashboard', [AdminController::class, 'admin'])->name('admin.dashboard');
     Route::get('/admin_profile', [AdminController::class, 'profile']);
     Route::put('/admin_profile', [AdminController::class, 'updateprofile']);
@@ -25,17 +34,12 @@ Route::middleware(['preventBackHistory','auth','userAccess:admin'])->group(funct
 
 //siswa
 Route::middleware(['preventBackHistory','auth','userAccess:siswa'])->group(function () {
-    
-Route::get('/absen', [SiswaController::class, 'siswa'])->name('siswa.absen');
-Route::get('/siswa_profile', [SiswaController::class, 'profile']);
-Route::put('/siswa_profile', [SiswaController::class, 'updateprofile']);
+    Route::get('/absen', [SiswaController::class, 'siswa'])->name('siswa.absen');
+    Route::get('/siswa_profile', [SiswaController::class, 'profile']);
+    Route::put('/siswa_profile', [SiswaController::class, 'updateprofile']);
 });
 
 Route::get('/datasiswa', [AdminController::class, 'data'])->name('admin.datasiswa');
-
-Route::get('/profile', [SiswaController::class, 'profilesiswa'])->name('siswa.profile');
-
 Route::get('/infoAbsen', [SiswaController::class, 'info'])->name('siswa.infoAbsen');
-
 Route::get('/pulang', [SiswaController::class, 'pulang'])->name('pulang');
 
